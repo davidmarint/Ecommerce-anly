6c05332df2ccee67ed2067febe8afcbbc84e276f
davidmarint <maicolmarin0126@gmail.com>
2025-05-21 14:58:04 -0500

    refactor: Update SignupUseCase to use generic Either type for improved flexibility

--- lib/domain/auth/useCase/signup.dart
+++ lib/domain/auth/useCase/signup.dart
@@ -4,9 +4,9 @@ import 'package:ecommerce_anly/data/auth/models/user_creation_req.dart';
 import 'package:ecommerce_anly/domain/auth/repository/auth.dart';
 import 'package:ecommerce_anly/service_locator.dart';
 
-class SignupUseCase implements Usecase<Either<String, String>, UserCreationReq> {
+class SignupUseCase implements Usecase<Either, UserCreationReq> {
   @override
-  Future<Either<String, String>> call({UserCreationReq ? params}) async {
+  Future<Either> call({UserCreationReq ? params}) async {
     return await sl<AuthRepository>().signUp(params!); 
   }
 }
